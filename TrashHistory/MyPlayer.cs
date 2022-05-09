7018a64223760fc3f1575a3742c5f905678e1027
hamstar0 <webutek@gmail.com>
2022-05-09 08:43:41 -0700

    v1.0.0.1 - Fixed trash history save/load

--- TrashHistory/MyPlayer.cs
+++ TrashHistory/MyPlayer.cs
@@ -8,11 +8,6 @@ using Terraria.ModLoader.IO;
 
 namespace TrashHistory {
 	public partial class TrashHistoryPlayer : ModPlayer {
-		public int TrashHistoryCount => this.TrashStore.Count;
-
-
-		////////////////
-
 		private List<Item> TrashStore = new List<Item>();
 
 		private bool IsTrashStacked = true;
@@ -22,11 +17,21 @@ namespace TrashHistory {
 		private Item LastSeenTrashSlotItem = null;
 
 
+		////////////////
+
+		public int TrashHistoryCount => this.TrashStore.Count;
+
+		////
+
+		public override bool CloneNewInstances => false;
+
+
 
 		////////////////
 
 		public override void Initialize() {
 			this.TrashStore.Clear();
+			this.LastSeenTrashSlotItem = null;
 			this.IsTrashStacked = true;
 		}
 
@@ -35,34 +40,63 @@ namespace TrashHistory {
 
 		public override void Load( TagCompound tag ) {
 			this.TrashStore.Clear();
+			this.LastSeenTrashSlotItem = null;
+			this.IsTrashStacked = true;
 
 			//
 
-			if( !tag.ContainsKey("history_count") ) {
+			if( !tag.ContainsKey("trash_history_count") ) {
 				return;
 			}
 
 			//
 
-			int count = tag.GetInt( "history_count" );
+			int count = tag.GetInt( "trash_history_count" );
 
 			for( int i=0; i<count; i++ ) {
-				Item item = tag.Get<Item>( $"history_{i}" );
+				TagCompound itemTag = tag.GetCompound( $"trash_history_{i}" );
 
-				this.TrashStore.Add( item );
+				this.TrashStore.Add( ItemIO.Load(itemTag) );
 			}
+
+			//
+
+			bool hasTrashItem = false;
+
+			if( tag.ContainsKey("has_trash_item") ) {
+				hasTrashItem = tag.GetBool( "has_trash_item" );
+
+				if( hasTrashItem ) {
+					TagCompound itemTag = tag.GetCompound( "trash_item" );
+
+					this.player.trashItem = ItemIO.Load( itemTag );
+				}
+			}
+
+			//
+
+			this.LastSeenTrashSlotItem = this.player.trashItem;
 		}
 
 
 		public override TagCompound Save() {
 			int count = this.TrashStore.Count;
 
-			var tag = new TagCompound { { "history_count", count } };
+			var tag = new TagCompound { { "trash_history_count", count } };
 
 			//
 
 			for(int i=0; i<count; i++ ) {
-				tag[ $"history_{i}" ] = this.TrashStore[i];
+				tag[ $"trash_history_{i}" ] = ItemIO.Save( this.TrashStore[i] );
+			}
+
+			//
+
+			bool hasTrashItem = this.player.trashItem?.active == true && !this.player.trashItem.IsAir;
+
+			tag["has_trash_item"] = hasTrashItem;
+			if( hasTrashItem ) {
+				tag["trash_item"] = ItemIO.Save( this.player.trashItem );
 			}
 
 			//
